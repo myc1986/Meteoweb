99bad6a87c6f8a41234f442df965c3fed39a12cf
myc1986 <moratinos.c@gmail.com>
2016-08-20 00:10:32 +0200

    init

---
+++ MeteoWeb/Service1.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MeteoWeb
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class MeteoTestWeb : IServiceMeteo
+    {
+        public string GetHello()
+        {
+            return "Hello";
+        }
+    }
+}
